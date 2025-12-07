d80ed57cb3a68fed2d5a4e9032ae7647c2f4acf4
Yuri Borges <yurib_n@hotmail.com>
2025-12-06 22:21:20 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -20,6 +20,17 @@ class Personagem:
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNivel: {self.get_nivel()}"
 
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida <= 0:
+            self.__vida = 0
+
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+
+
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
         super().__init__(nome, vida, nivel)
@@ -59,6 +70,16 @@ class Jogo:
 
             input("Pressione Enter para atacar...")
             escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial): ")
+
+            if escolha == '1':
+                self.heroi.atacar(self.inimigo)
+            else:
+                print("Escolha inválida. Escolha novamente.")
+
+        if self.heroi.get_vida() > 0:
+            print("Parabéns! VOcê venceu a batalha!")
+        else:
+            print("Você foi derrotado!")
 
 
 # Criar instância do jogo e iniciar batalha
